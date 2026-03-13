86208ed4043ee69668e97106fa361565803650ad
Sailor4 <ddimitrov8686@gmail.com>
2026-03-13 20:26:54 +0200

    add common

--- game_catalog/settings.py
+++ game_catalog/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+# My apps
+
+    'common',
 ]
 
 MIDDLEWARE = [
@@ -104,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Sofia'
 
 USE_I18N = True
 
