5acc23692bdf5a8263423045875f73bc4247f6e5
Sailor4 <ddimitrov8686@gmail.com>
2026-03-14 16:45:25 +0200

    home page fix

--- common/views.py
+++ common/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from games.models import Game
+from reviews.models import Review
 
 
 def index(request):
-    return render(request, 'common/index.html')
+    games_count = Game.objects.count()
+    reviews_count = Review.objects.count()
+    context = {
+        'games_count': games_count,
+        'reviews_count': reviews_count,
+    }
+
+    return render(request, 'common/index.html', context)
